3d0107b34659c19c7fa8da46f08cd26eca940089
UmaMahesh13 <pmahesh1357@gmail.com>
2023-07-01 14:27:44 +0530

    added posts to home view

--- ArticleWebsite/views.py
+++ ArticleWebsite/views.py
@@ -2,4 +2,5 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-    return render(request,"home.html")
+    posts=["This is "+str(i)+" post" for i in range(1,11)]
+    return render(request,"home.html",{"posts":posts})
