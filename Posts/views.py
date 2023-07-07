b197a5aa4c986170c38cc2f7056cf8bd2c98e88f
UmaMahesh13 <pmahesh1357@gmail.com>
2023-07-07 15:32:19 +0530

    added article_edit feature prototype

--- Posts/views.py
+++ Posts/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+def edit(request):
+    return render(request,"post_edit.html")
+# views.py
+
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+@csrf_exempt
+def auto_save_post(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        
+        # Update the corresponding article record or data object with the new information
+        # Save the changes to the database
+        
+        # Return a success response
+        return JsonResponse({'message': 'Auto-save successful'})
+    else:
+        # Return an error response if the request method is not POST
+        return JsonResponse({'message': 'Invalid request'}, status=400)
